fc0ad9037230cf9e92809e7ce9e0b0cb0d5af9ad
Davi Bernardo <sbernardo.davi@outlook.com>
2026-05-11 02:36:25 -0300

    small changes

--- admin/app/admin/layout.tsx
+++ admin/app/admin/layout.tsx
@@ -1,5 +1,4 @@
 import Navbar from "./navbar/Navbar";
-import Footer from "./footer/Footer";
 
 export default function AdminLayout({
   children,
--- admin/app/layout.tsx
+++ admin/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Footer from "./admin/footer/footer";
+import Footer from "./admin/footer/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +28,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-screen flex flex-col">{children}</body>
+      <body className="min-h-screen flex flex-col">
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
