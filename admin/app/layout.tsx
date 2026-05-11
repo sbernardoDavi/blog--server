e8e18d73cc0f8fba78628e0e35b5778634dc9c86
Davi Bernardo <sbernardo.davi@outlook.com>
2026-05-11 02:29:08 -0300

    fix: footer

--- admin/app/layout.tsx
+++ admin/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Footer from "./admin/footer/Footer";
 import "./globals.css";
+import Footer from "./admin/footer/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,10 +28,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-screen flex flex-col">
-        {children}
-        <Footer />
-      </body>
+      <body className="min-h-screen flex flex-col">{children}</body>
     </html>
   );
 }
